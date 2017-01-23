bd1a70443e4a009e863fc77357d6da5191baa2c4
chenlangping <328566090@qq.com>
2017-01-23 17:43:58 +0800

    获取图片，有点小问题

--- app/src/main/java/com/example/palmneu/LoginIn.java
+++ app/src/main/java/com/example/palmneu/LoginIn.java
@@ -1,11 +1,13 @@
 package com.example.palmneu;
 
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -22,13 +24,17 @@ public class LoginIn extends AppCompatActivity {
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
     private Button login;
+    private ImageView imageView;
     private URL url;
     private HttpURLConnection connection;
     private BufferedReader reader;
     private TextView responseText;
     private final String NEU = "http://202.118.31.197";
     private final String BAIDU = "http://www.baidu.com";
-    private String cookie=null;
+    private String cookie = null;
+    private String picturesrc ="";
+    private String htmlcode = "";
+    private String pictureurl="";
 
 
     @Override
@@ -41,18 +47,14 @@ public class LoginIn extends AppCompatActivity {
         passwordEdit = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login_in);
         responseText = (TextView) findViewById(R.id.response_text);
+        imageView = (ImageView) findViewById(R.id.check_picture);
         accountEdit.setText(preferences.getString("account", ""));
         passwordEdit.setText(preferences.getString("password", ""));
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendRequestWithHttpURLConnection();
-            }
-        });
-
+        getcookieandpicturesrc();//获取cookie和验证码图片的地址
+        getPicture();
     }
 
-    private void sendRequestWithHttpURLConnection() {
+    private void getcookieandpicturesrc() {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -71,19 +73,22 @@ public class LoginIn extends AppCompatActivity {
                     while ((line = reader.readLine()) != null) {
                         response.append(line);
                     }
-                    //response中存的就是返回的html页面的代码
+                    htmlcode = response.toString();
                     StringBuilder responsehead = new StringBuilder();
                     String key;
                     for (int i = 1; (key = connection.getHeaderFieldKey(i)) != null; i++) {
-                        responsehead.append(key+":"+connection.getHeaderField(key));
-                        if(key.equals("Set-Cookie")){
-                            cookie=connection.getHeaderField(key);
+                        responsehead.append(key + ":" + connection.getHeaderField(key));
+                        if (key.equals("Set-Cookie")) {
+                            cookie = connection.getHeaderField(key);
                         }
                     }
-                    cookie=cookie.substring(0,cookie.length()-8);
+                    cookie = cookie.substring(0, cookie.length() - 8);
+                    //TODO 存在隐患！
                     //responsehead中存的是响应头的数据
                     //showResponse(response.toString());
-                    showResponse(cookie);
+                    picturesrc = htmlcode.substring(htmlcode.indexOf("ACTIONVALIDATERANDOMPICTURE"), htmlcode.indexOf("ACTIONVALIDATERANDOMPICTURE") + 64);
+                    picturesrc = picturesrc.substring(0, picturesrc.indexOf("\""));
+                    //showResponse(picturesrc);
 
 
                 } catch (Exception e) {
@@ -114,5 +119,42 @@ public class LoginIn extends AppCompatActivity {
         });
     }
 
+    private void getPicture() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                Bitmap bitmap = null;
 
+                try {
+                    pictureurl=NEU+"/"+picturesrc;
+                    URL url = new URL("http://202.118.31.197/ACTIONVALIDATERANDOMPICTURE.APPPROCESS?id=9.377868860400651");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    bitmap = BitmapFactory.decodeStream(in);
+                    showPicture(bitmap);
+                    showResponse(picturesrc);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void showPicture(final Bitmap bitmap){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                imageView.setImageBitmap(bitmap);
+                //处理
+            }
+        });
+    }
 }

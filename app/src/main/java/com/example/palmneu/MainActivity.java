60aec023fa77561c06cb5fbbe5f01cfba346588a
chenlangping <328566090@qq.com>
2017-02-11 15:01:13 +0800

    主活动中增加了地图按钮

--- app/src/main/java/com/example/palmneu/MainActivity.java
+++ app/src/main/java/com/example/palmneu/MainActivity.java
@@ -29,5 +29,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button map =(Button)findViewById(R.id.position_data);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Map.class);
+                startActivity(intent);
+            }
+        });
     }
 }

8ea55f3bb9a795f2910d93c44f74887864c4892f
chenlangping <328566090@qq.com>
2017-03-17 16:02:10 +0800

    登录和注册的页面

---
+++ app/src/main/java/com/example/palmneu/UserLogin.java
@@ -0,0 +1,46 @@
+package com.example.palmneu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class UserLogin extends AppCompatActivity {
+
+    private EditText accountEdit;//账号编辑框
+    private EditText passwordEdit;//密码编辑框
+    private Button login;//登录按钮
+    private Button register;//注册按钮
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+        initView();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //执行登录操作
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //执行注册操作
+            }
+        });
+    }
+
+
+    private void initView(){
+
+        accountEdit=(EditText)findViewById(R.id.account);
+        passwordEdit=(EditText)findViewById(R.id.password);
+        login=(Button)findViewById(R.id.login);
+        register=(Button)findViewById(R.id.register);
+
+    }
+}

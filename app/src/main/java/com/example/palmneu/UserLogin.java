8d48f498e84043facf346f55a22cea812accc3e2
chenlangping <328566090@qq.com>
2017-03-20 16:48:26 +0800

    基本把注册功能做完了。当用户输入有重复的时候会提示，注册成功则直接跳到登录界面。注册和登录的check函数仍然需要完善。

--- app/src/main/java/com/example/palmneu/UserLogin.java
+++ app/src/main/java/com/example/palmneu/UserLogin.java
@@ -3,9 +3,17 @@ package com.example.palmneu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class UserLogin extends AppCompatActivity {
@@ -14,31 +22,28 @@ public class UserLogin extends AppCompatActivity {
     private EditText passwordEdit;//密码编辑框
     private Button login;//登录按钮
     private Button register;//注册按钮
+    String account=null;//用户名
+    String password=null;//密码
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
         initView();
 
         login.setOnClickListener(new View.OnClickListener() {
-            String account;//用户名
-            String password;//密码
-            boolean result = false;//登录状态
-
             @Override
             public void onClick(View v) {
                 //获取输入内容
                 account = accountEdit.getText().toString();
                 password = passwordEdit.getText().toString();
                 if (check(account, password)) {
-                    //暂定向服务器发送的消息格式为   LOGIN%username%password
-                    sendMessage("LOGIN%" + account + "%" + password);
+                    sendLoginMessageToServer(account,password);
                 } else {
-
+                    showToast("输入错误");
                 }
-
 
             }
         });
@@ -48,12 +53,11 @@ public class UserLogin extends AppCompatActivity {
             public void onClick(View v) {
                 //执行注册操作
                 Intent intent = new Intent(UserLogin.this, UserRegister.class);
-                startActivity(intent);
+                startActivityForResult(intent,1);
             }
         });
 
     }
-
 
     private void initView() {
 
@@ -65,13 +69,6 @@ public class UserLogin extends AppCompatActivity {
     }
 
 
-    /**
-     * 检测是否是有效输入
-     *
-     * @param username
-     * @param password
-     * @return flag
-     */
     private boolean check(String username, String password) {
         boolean flag = false;
         if (username != null && password != null) {
@@ -80,12 +77,58 @@ public class UserLogin extends AppCompatActivity {
         return flag;
     }
 
-    /**
-     * 向服务器发送信息方法
-     * @param message
-     */
-    private void sendMessage(String message){
+    private void sendLoginMessageToServer(final String account,final String password){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
 
+                try{
+                    OkHttpClient client=new OkHttpClient();
+                    RequestBody requestBody=new FormBody.Builder()
+                            .add("userName",account)
+                            .add("passWord",password)
+                            .build();
+
+                    Request request=new Request.Builder()
+                            .url(new DataClass().serveraddress+"login.php")
+                            .post(requestBody)
+                            .build();
+
+                    Response response= client.newCall(request).execute();
+                    String responseData =response.body().string();
+                    Log.d("clp","返回信息:"+responseData);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+    }
+
+    private void showToast(String msg){
+        Toast.makeText(UserLogin.this,msg,Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 1:
+                if(requestCode==1){
+                    //用户注册之后返回的
+                    String account=data.getStringExtra("userName");
+                    String password=data.getStringExtra("passWord");
+                    accountEdit.setText(account);
+                    passwordEdit.setText(password);
+                }else if(requestCode==2){
+                    //用户只是按了返回键返回，什么都不用做
+                }
+
+                break;
+            default:
+                //没有别的了
+        }
     }
 }
 
--- app/src/main/java/com/example/palmneu/UserRegister.java
+++ app/src/main/java/com/example/palmneu/UserRegister.java
@@ -1,11 +1,13 @@
 package com.example.palmneu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
@@ -45,7 +47,7 @@ public class UserRegister extends AppCompatActivity {
                 }else{
                     //告知用户错误信息
                 }
-                //userName=chenlangping&nickName=chenlangping&emailAddress=328566090@qq.com&passWord=123456
+
 
 
             }
@@ -69,6 +71,7 @@ public class UserRegister extends AppCompatActivity {
             @Override
             public void run() {
 
+                //userName=chen&passWord=1234&nickName=123jeo&emailAddress=123@163.com
                 try{
                     OkHttpClient client=new OkHttpClient();
                     RequestBody requestBody=new FormBody.Builder()
@@ -85,13 +88,56 @@ public class UserRegister extends AppCompatActivity {
 
                     Response response= client.newCall(request).execute();
                     String responseData =response.body().string();
-                    Log.d("register:","返回信息:"+responseData);
+                    Log.d("clp","返回信息:"+responseData);
+
+
+                    if (responseData.indexOf("0") != -1) {
+                        //返回0，表示成功注册
+                        toastShow("注册成功");
+                        //注册成功就返回登录界面，并且帮助用户填上账号密码
+                        Intent intent=new Intent();
+                        intent.putExtra("userName",account);
+                        intent.putExtra("passWord",password);
+                        setResult(1,intent);//1代表的是正确注册的结果
+                        finish();
+                    }else if (responseData.indexOf("1") != -1) {
+                        toastShow("email地址已经被注册");
+                    }else if (responseData.indexOf("2") != -1) {
+                        toastShow("昵称已被注册");
+                    }else if (responseData.indexOf("3") != -1) {
+                        toastShow("昵称和email地址都被注册了");
+                    }else if (responseData.indexOf("4") != -1) {
+                        toastShow("用户名已存在");
+                    }else if (responseData.indexOf("5") != -1) {
+                        toastShow("用户名和email地址均已存在");
+                    }else if (responseData.indexOf("6") != -1) {
+                        toastShow("用户名和昵称均已存在");
+                    }else if (responseData.indexOf("7") != -1) {
+                        toastShow("用户名和昵称和email地址均已存在！");
+                    }
                 }catch (Exception e){
                     e.printStackTrace();
                 }
 
             }
         }).start();
+    }
+
+    private void toastShow(final String msg) {//提示用户无法连接到校园网
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(UserRegister.this,msg,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        //如果用户没有注册而是通过返回键返回的，那么返回值是2
+        Intent intent=new Intent();
+        setResult(2,intent);
+        finish();
     }
 }
 

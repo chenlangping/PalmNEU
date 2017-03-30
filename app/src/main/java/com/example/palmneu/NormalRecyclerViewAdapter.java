b3e7642e1c992f39fb76b3485cf3fc95672fc50c
陈瑜峰 <657890261@qq.com>
2017-03-30 21:07:05 +0800

    陈瑜峰上传看帖功能相关.java文件三个和两个xml

---
+++ app/src/main/java/com/example/palmneu/NormalRecyclerViewAdapter.java
@@ -0,0 +1,73 @@
+package com.example.palmneu;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+/**
+ * Created by chenyufeng on 17/3/30.
+ */
+
+public class NormalRecyclerViewAdapter extends RecyclerView.Adapter<NormalRecyclerViewAdapter.NormalTextViewHolder> {
+
+
+    //贴子内容字符串数组
+    private String[] noteMessage = null;
+
+    public NormalRecyclerViewAdapter(String[] noteMessage) {
+        this.noteMessage = noteMessage;
+    }
+
+    @Override
+    public NormalTextViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        return new NormalTextViewHolder(mLayoutInflater.inflate(R.layout.activity_note, parent, false));
+        View view= LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item,parent,false);
+        NormalTextViewHolder normalTextViewHolder=new NormalTextViewHolder(view);
+        return normalTextViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(NormalTextViewHolder holder, int position) {
+        holder.ID.setText(noteMessage[position].split("#")[0]);
+        holder.userName.setText(noteMessage[position].split("#")[1]);
+        holder.time.setText(noteMessage[position].split("#")[2]);
+        holder.noteTitle.setText(noteMessage[position].split("#")[3]);
+        holder.noteContent.setText(noteMessage[position].split("#")[4]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return noteMessage == null ? 0 : noteMessage.length;
+    }
+
+    static class NormalTextViewHolder extends RecyclerView.ViewHolder {
+
+        TextView ID;
+        TextView userName;
+        TextView time;
+        TextView noteTitle;
+        TextView noteContent;
+
+        public NormalTextViewHolder(View itemView) {
+            super(itemView);
+            ID = (TextView) itemView.findViewById(R.id.note_ID);
+            userName = (TextView) itemView.findViewById(R.id.note_userName);
+            time = (TextView) itemView.findViewById(R.id.note_time);
+            noteTitle = (TextView) itemView.findViewById(R.id.note_noteTitle);
+            noteContent = (TextView) itemView.findViewById(R.id.note_noteContent);
+
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/palmneu/Note.java
+++ app/src/main/java/com/example/palmneu/Note.java
@@ -3,9 +3,18 @@ package com.example.palmneu;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
@@ -15,11 +24,14 @@ import okhttp3.Response;
 
 public class Note extends AppCompatActivity {
 
-    private ListView listView = null;
+//    private ListView listView = null;
+
+    private RecyclerView mRecyclerView = null;
 
     private ArrayAdapter<String> adapter = null;
 
-    String []noteMessage=null;
+    String[] noteMessage = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,45 +39,54 @@ public class Note extends AppCompatActivity {
 
         //SharedPreferences pref=getSharedPreferences("data",MODE_PRIVATE);
         //int id= pref.getInt("ID",0);
-        listView=(ListView)findViewById(R.id.list_view);
-
+        //listView = (ListView) findViewById(R.id.list_view);
 
         getLatestNote();
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));//这里用线性显示 类似于listview
+        mRecyclerView.setAdapter(new NormalRecyclerViewAdapter(noteMessage));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL_LIST));
 
     }
 
-    private void getLatestNote(){
+    private void getLatestNote() {
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
 
-                    Log.d("clp","开始传输信息");
-                    OkHttpClient client =new OkHttpClient();
+//                    Log.d("clp", "开始传输信息");
+//                    OkHttpClient client = new OkHttpClient();
+//
+//                    SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+//                    String ID = pref.getString("ID", "0");
+//
+//                    RequestBody requestBody = new FormBody.Builder()
+//                            .add("ID", ID)
+//                            .build();
+//
+//                    Request request = new Request.Builder()
+//                            .url(new DataClass().serveraddress + "push_note.php")
+//                            .post(requestBody)
+//                            .build();
+//
+//                    Response response = client.newCall(request).execute();
+//                    String responseDate = response.body().string();
 
-                    SharedPreferences pref=getSharedPreferences("data",MODE_PRIVATE);
-                    String ID= pref.getString("ID","0");
 
-                    RequestBody requestBody = new FormBody.Builder()
-                            .add("ID", ID)
-                            .build();
+                    //测试用字符串
+                    String responseDate ="ID#userName#time#noteTitle#noteContent@ID#userName#time#noteTitle#noteContent@ID#userName#time#noteTitle#noteContent@ID#userName#time#noteTitle#noteContent@ID#userName#time#noteTitle#noteContent@ID#userName#time#noteTitle#noteContent@ID#userName#time#noteTitle#noteContent@ID#userName#time#noteTitle#noteContent@ID#userName#time#noteTitle#noteContent@ID#userName#time#noteTitle#noteContent@";
 
-                    Request request = new Request.Builder()
-                            .url(new DataClass().serveraddress + "push_note.php")
-                            .post(requestBody)
-                            .build();
+                    noteMessage = responseDate.split("@");
+                    Log.d("clp", responseDate);
 
-                    Response response = client.newCall(request).execute();
-                    String responseDate=response.body().string();
 
-                    noteMessage=responseDate.split("@");
-                    Log.d("clp",responseDate);
+//                    showNoteMessage();
 
-                    showNoteMessage();
-
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -74,14 +95,16 @@ public class Note extends AppCompatActivity {
 
     }
 
-    private void showNoteMessage() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                //更新
-                adapter = new ArrayAdapter<String>(Note.this, android.R.layout.simple_list_item_1, noteMessage);
-                listView.setAdapter(adapter);
-            }
-        });
-    }
+//    private void showNoteMessage() {
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                //更新
+////                adapter = new ArrayAdapter<String>(Note.this, android.R.layout.simple_list_item_1, noteMessage);
+////                listView.setAdapter(adapter);
+//                  mRecyclerView.postInvalidate();
+//
+//            }
+//        });
+//    }
 }

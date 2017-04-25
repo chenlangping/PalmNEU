ef83ff03db6f6ac5ffd4e0c2131314bc00fc3cfa
陈瑜峰 <657890261@qq.com>
2017-04-25 22:32:08 +0800

    陈瑜峰上传发帖中从相册选择图片功能

--- app/src/main/java/com/example/palmneu/SendNote.java
+++ app/src/main/java/com/example/palmneu/SendNote.java
@@ -1,14 +1,20 @@
 package com.example.palmneu;
 
+import android.annotation.TargetApi;
 import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
@@ -24,6 +30,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.jar.Manifest;
 
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
@@ -34,6 +41,7 @@ import okhttp3.Response;
 public class SendNote extends AppCompatActivity {
 
     public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
     private String userName = "";
     private String passWord = "";
     //用户名和密码
@@ -93,7 +101,6 @@ public class SendNote extends AppCompatActivity {
                 .inflate(R.menu.addpic, popup.getMenu());
 
 
-
         //设置item的点击事件
         popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
@@ -121,6 +128,12 @@ public class SendNote extends AppCompatActivity {
                         startActivityForResult(intent, TAKE_PHOTO);
                         return true;
                     case R.id.action_album:
+                        if (ContextCompat.checkSelfPermission(SendNote.this,
+                                android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(SendNote.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                        } else {
+                            openAlbum();
+                        }
                         return true;
                     default:
                         return false;
@@ -129,34 +142,121 @@ public class SendNote extends AppCompatActivity {
             }
         });
 
+
         popup.show(); //showing popup menu
+    }
+
+
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);//打开相册
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             case TAKE_PHOTO:
-                Log.d("clp","test121");
+                Log.d("clp", "test121");
                 if (resultCode == RESULT_OK) {
-                    Log.d("clp","test11");
+                    Log.d("clp", "test11");
                     try {
-                        Log.d("clp","test113");
+                        Log.d("clp", "test113");
                         //将拍摄的照片显示出来
-                        Bitmap bitmap=null;
+                        Bitmap bitmap = null;
                         bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
-                        Log.d("clp",String.valueOf(bitmap));
+                        Log.d("clp", String.valueOf(bitmap));
 //                        pic1.setImageBitmap(bitmap);
                         showPicture(bitmap);
                     } catch (Exception e) {
                         e.printStackTrace();
-                        Log.d("clp","test122221");
+                        Log.d("clp", "test122221");
                     }
                 }
                 break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    //判断手机系统版本号
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        //4.4及以上系统使用这个方法处理图片
+                        handleImageOnKitKat(data);
+                    } else {
+                        //4.4及以下系统使用这个方法处理图片
+                        handleImageBeforeKitKat(data);
+                    }
+                    break;
+                }
             default:
                 break;
         }
     }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            //如果是document类型的Uri，则通过document id 处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];//解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+                //如果是content类型的Uri,则使用普通方式处理
+                imagePath = getImagePath(uri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            //如果是content类型的Uri,则使用普通方式处理
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);//根据图片路径显示图片
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        //通过Uri和selection来获取真实的照片路径
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            pic2.setImageBitmap(bitmap);
+        } else {
+            Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void showPicture(final Bitmap bitmap) {
         runOnUiThread(new Runnable() {
             @Override
